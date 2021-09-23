95a3b743b0a74181fe8103823eaf0916a6f8d6ac
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-09-23 20:38:54 +0900

    change to docker hub

--- scripts/push_image.bash
+++ scripts/push_image.bash
@@ -7,15 +7,10 @@ base_dir="$(
 
 echo "----- build -----"
 image_name="djangogirls"
+registry_image_name="$DOCKER_HUB_USERNAME/$image_name"
 
-docker build --no-cache -t "$image_name" "$base_dir"
+docker build --no-cache -t "$registry_image_name" "$base_dir"
 
 echo "----- push -----"
-account_id="$(aws sts get-caller-identity --query Account --output text)"
-region="${AWS_DEFAULT_REGION:-"ap-northeast-1"}"
-ecr_uri="$account_id.dkr.ecr.$region.amazonaws.com"
-ecr_image_name="$ecr_uri/$image_name"
-
-aws ecr get-login-password | docker login --username AWS --password-stdin "https://$ecr_uri"
-docker tag "$image_name" "$ecr_image_name"
-docker push "$ecr_image_name"
+echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+docker push "$registry_image_name"
