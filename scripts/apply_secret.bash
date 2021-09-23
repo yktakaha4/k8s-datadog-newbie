b66565bdcf918ae0d33223d2d9bc815dee09f470
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-09-24 03:18:58 +0900

    fix

---
+++ scripts/apply_secret.bash
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+
+namespace="k8s-datadog-newbie"
+secret_name="k8s-datadog-newbie-secret"
+
+kubectl delete secret -n "$namespace" "$secret_name" 2>/dev/null || echo "skip delete."
+kubectl create secret generic -n "$namespace" "$secret_name" \
+  --from-literal="DATADOG_API_KEY=$DATADOG_API_KEY"
--- scripts/push_image.bash
+++ scripts/push_image.bash
@@ -1,16 +1,12 @@
 #!/bin/bash -eu
 
-base_dir="$(
-  cd "$(dirname "$0")/.."
-  pwd
-)"
-
-echo "----- build -----"
-image_name="djangogirls"
+docker_dir="$1"
+image_name="$(basename "$docker_dir")"
 registry_image_name="$DOCKER_HUB_USERNAME/$image_name"
 
-docker build --no-cache -t "$registry_image_name" "$base_dir"
+echo "----- build: $registry_image_name -----"
+docker build --no-cache -t "$registry_image_name" "$docker_dir"
 
-echo "----- push -----"
+echo "----- push: $registry_image_name -----"
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 docker push "$registry_image_name"
