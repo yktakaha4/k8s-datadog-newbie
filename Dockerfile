fd8375d945952c69e23e0a507b6ef3bf45eb53e0
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-09-23 20:05:27 +0900

    add push script

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.7.12-slim-buster
+
+# Install Node.js
+RUN apt-get -y update \
+  && apt-get install -y curl gnupg \
+  && curl -sL https://deb.nodesource.com/setup_14.x | bash - \
+  && apt-get install -y nodejs \
+  && npm install npm@latest -g
+
+COPY ./djangogirls /djangogirls
+
+WORKDIR /djangogirls
+
+RUN pip install pip-tools \
+  && pip-sync \
+  && npm install \
+  && npx gulp build
---
+++ scripts/push_image.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+base_dir="$(
+  cd "$(dirname "$0")/.."
+  pwd
+)"
+
+echo "----- build -----"
+image_name="djangogirls"
+
+docker build -t "$image_name" "$base_dir"
+
+echo "----- push -----"
+account_id="$(aws sts get-caller-identity --query Account --output text)"
+region="${AWS_DEFAULT_REGION:-"ap-northeast-1"}"
+ecr_uri="$account_id.dkr.ecr.$region.amazonaws.com"
+ecr_image_name="$ecr_uri/$image_name"
+
+aws ecr get-login-password | docker login --username AWS --password-stdin "https://$ecr_uri"
+docker tag "$image_name" "$ecr_image_name"
+docker push "$ecr_image_name"
